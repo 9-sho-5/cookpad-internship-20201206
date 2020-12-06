8cacbc27a31175f9b429af67f6047ac8ced17441
Hosho Kusunose <hosho.kusu@icloud.com>
2020-12-06 13:58:58 +0900

    TDD

---
+++ spec/poll_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/poll'
+
+
+RSpec.describe Poll do
+    it 'has a title and candidates' do
+        poll = Poll.new('Awesome Poll', ['Alice', 'Bob']) # %w[Alice Bob]
+
+        expect(poll.title).to eq 'Awesome Poll'
+        expect(poll.candidates).to eq ['Alice', 'Bob']
+    end
+end
