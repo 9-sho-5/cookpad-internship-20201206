41b863fcddd6973057dd1444a98018226b102ec9
Hosho Kusunose <hosho.kusu@icloud.com>
2020-12-06 16:01:20 +0900

    vote

--- lib/poll.rb
+++ lib/poll.rb
@@ -17,4 +17,19 @@ class Poll
 
     @votes.push(vote)
   end
+
+  def count_votes
+
+    result = {}
+
+    candidates.each do |candidate|
+        result[candidate] = 0
+    end
+
+    votes.each do |vote|
+        result[vote.candidate] += 1
+    end
+
+    result.sort_by{|_key, value| value}.reverse.to_h
+  end
 end
--- spec/poll_spec.rb
+++ spec/poll_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/poll'
 
+
 RSpec.describe Poll do
   it 'has a title and candidates' do
     poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
@@ -25,6 +26,33 @@ RSpec.describe Poll do
 
         expect { poll.add_vote(vote) }.to raise_error Poll::InvalidCandidateError
       end
+    end
+  end
+
+  describe '#count_votes' do
+    it 'counts the votes and sorts the candidates by the number of votes' do
+        poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+        poll.add_vote(Vote.new('Carol', 'Alice'))
+        poll.add_vote(Vote.new('Dave', 'Alice'))
+        poll.add_vote(Vote.new('Ellen', 'Bob'))
+
+        result = poll.count_votes
+
+        expect(result['Alice']).to eq 2
+
+        expect(result['Bob']).to eq 1
+
+        expect(result.keys).to eq ['Alice', 'Bob']
+
+        poll2 = Poll.new('Great Poll', ['Alice', 'Bob'])
+        poll2.add_vote(Vote.new('Carol', 'Bob'))
+        poll2.add_vote(Vote.new('Dave', 'Bob'))
+
+        result2 = poll2.count_votes
+
+        expect(result2['Alice']).to eq 0
+        expect(result2['Bob']).to eq 2
+        expect(result2.keys).to eq ['Bob', 'Alice']
     end
   end
 end
