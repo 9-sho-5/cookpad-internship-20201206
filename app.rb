7d151ce7711de19a3a95e4b3b07cfa20121009e4
Hosho Kusunose <hosho.kusu@icloud.com>
2020-12-06 16:19:00 +0900

    index

--- app.rb
+++ app.rb
@@ -1,1 +1,12 @@
 require 'sinatra'
+require_relative 'lib/poll'
+require_relative 'lib/vote'
+
+get '/' do
+    polls = [
+        Poll.new('好きな料理', ['肉じゃが', '生姜焼き', '唐揚げ']),
+        Poll.new('人気投票', ['おむすびけん','クックパッドたん']),
+    ]
+    # 配列pollsをindexで使用できるように変数指定
+    erb :index, locals: {polls: polls}
+end
