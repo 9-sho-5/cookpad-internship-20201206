04b31649c9c512bea39e02fbbd13ef5a64007e45
Hosho Kusunose <hosho.kusu@icloud.com>
2020-12-06 14:20:36 +0900

    practice1

--- lib/poll.rb
+++ lib/poll.rb
@@ -1,6 +1,11 @@
 class Poll
     attr_reader :title, :candidates, :dates
 
+    # attr_reader と同じ
+    # def title
+    #     @title
+    # end
+
     def initialize(title, candidates, dates)
         @title = title
         @candidates = candidates
