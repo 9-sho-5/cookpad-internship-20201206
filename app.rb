06c9d04917d56378dcec724a13f1e53c8cc7ecb8
Hosho Kusunose <hosho.kusu@icloud.com>
2020-12-06 16:32:33 +0900

    detail poll

--- app.rb
+++ app.rb
@@ -1,11 +1,28 @@
 require 'sinatra'
+# サーバーを再起動しなくとも、コードの変更が自動的に反映される
+require 'sinatra/reloader' if development?
 require_relative 'lib/poll'
 require_relative 'lib/vote'
 
-get '/' do
-  polls = [
+$polls = [
     Poll.new('好きな料理', ['肉じゃが', 'しょうが焼き', 'から揚げ']),
     Poll.new('人気投票', ['おむすびけん', 'クックパッドたん']),
-  ]
-  erb :index, locals: { polls: polls }
+]
+
+get '/' do
+#   polls = [
+#     Poll.new('好きな料理', ['肉じゃが', 'しょうが焼き', 'から揚げ']),
+#     Poll.new('人気投票', ['おむすびけん', 'クックパッドたん']),
+#   ]
+#   erb :index, locals: { polls: polls }
+    erb :index, locals: { polls: $polls }
+end
+
+get '/polls/:id' do
+    index = params['id'].to_i
+    poll = $polls[index]
+    # ルーティング内でただちにリクエストを止めることができます
+    halt 404, '投票が見つかりませんでした' if poll.nil?
+
+    erb :poll, locals: { index: index, poll: poll}
 end
