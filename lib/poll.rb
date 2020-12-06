90b397f30bd95e1b16298dc1f46ae14d81b045cf
Hosho Kusunose <hosho.kusu@icloud.com>
2020-12-06 14:18:18 +0900

    practice1

--- lib/poll.rb
+++ lib/poll.rb
@@ -1,12 +1,9 @@
 class Poll
-    def initialize(title, candidates)
-    end
+    attr_reader :title, :candidates, :dates
 
-    def title
-        'Awesome Poll'
-    end
-
-    def candidates
-        ['Alice','Bob']
+    def initialize(title, candidates, dates)
+        @title = title
+        @candidates = candidates
+        @dates = dates
     end
 end
